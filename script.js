b677f6b68805cd35131dcc9768ebfa4533133ad6
Hiroki Ohara <latelucet@gmail.com>
2022-02-19 17:27:40 +0900

    first

--- script.js
+++ script.js
@@ -1,32 +1,56 @@
+let cx, cy;
+let secondsRadius;
+let minutesRadius;
+let hoursRadius;
+let clockDiameter;
 
- // this variable will hold our shader object
- let theShader;
- // this variable will hold our webcam video
- let cam;
+function setup() {
+  createCanvas(720, 400);
+  stroke(255);
 
- function preload(){
-   // load the shader
-   theShader = loadShader('assets/webcam.vert', 'assets/webcam.frag');
- }
+  let radius = min(width, height) / 2;
+  secondsRadius = radius * 0.71;
+  minutesRadius = radius * 0.6;
+  hoursRadius = radius * 0.5;
+  clockDiameter = radius * 1.7;
 
- function setup() {
-   // shaders require WEBGL mode to work
-   createCanvas(710, 400, WEBGL);
-   noStroke();
+  cx = width / 2;
+  cy = height / 2;
+}
 
-   cam = createCapture(VIDEO);
-   cam.size(710, 400);
+function draw() {
+  background(230);
 
-   cam.hide();
- }
+  // Draw the clock background
+  noStroke();
+  fill(244, 122, 158);
+  ellipse(cx, cy, clockDiameter + 25, clockDiameter + 25);
+  fill(237, 34, 93);
+  ellipse(cx, cy, clockDiameter, clockDiameter);
 
- function draw() {
-   // shader() sets the active shader with our shader
-   shader(theShader);
+  // Angles for sin() and cos() start at 3 o'clock;
+  // subtract HALF_PI to make them start at the top
+  let s = map(second(), 0, 60, 0, TWO_PI) - HALF_PI;
+  let m = map(minute() + norm(second(), 0, 60), 0, 60, 0, TWO_PI) - HALF_PI;
+  let h = map(hour() + norm(minute(), 0, 60), 0, 24, 0, TWO_PI * 2) - HALF_PI;
 
-   // passing cam as a texture
-   theShader.setUniform('tex0', cam);
+  // Draw the hands of the clock
+  stroke(255);
+  strokeWeight(1);
+  line(cx, cy, cx + cos(s) * secondsRadius, cy + sin(s) * secondsRadius);
+  strokeWeight(2);
+  line(cx, cy, cx + cos(m) * minutesRadius, cy + sin(m) * minutesRadius);
+  strokeWeight(4);
+  line(cx, cy, cx + cos(h) * hoursRadius, cy + sin(h) * hoursRadius);
 
-   // rect gives us some geometry on the screen
-   rect(0,0,width,height);
- }
+  // Draw the minute ticks
+  strokeWeight(2);
+  beginShape(POINTS);
+  for (let a = 0; a < 360; a += 6) {
+    let angle = radians(a);
+    let x = cx + cos(angle) * secondsRadius;
+    let y = cy + sin(angle) * secondsRadius;
+    vertex(x, y);
+  }
+  endShape();
+}
